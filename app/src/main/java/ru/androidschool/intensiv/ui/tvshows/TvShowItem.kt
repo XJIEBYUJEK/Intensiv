9abb4870302e573796d7b827afab57bfaaf69909
Igor Semenov <semenov@rnd.stcnet.ru>
2024-08-06 15:07:53 +0300

    TvShowsFragment

---
+++ app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowItem.kt
@@ -0,0 +1,31 @@
+package ru.androidschool.intensiv.ui.tvshows
+
+import android.view.View
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.viewbinding.BindableItem
+import ru.androidschool.intensiv.R
+import ru.androidschool.intensiv.data.Movie
+import ru.androidschool.intensiv.databinding.ItemTvShowBinding
+
+class TvShowItem(
+    private val content: Movie,
+    private val onClick: (movie: Movie) -> Unit
+) : BindableItem<ItemTvShowBinding>() {
+
+    override fun getLayout(): Int = R.layout.item_tv_show
+
+    override fun bind(view: ItemTvShowBinding, position: Int) {
+        view.description.text = content.title
+        view.tvShowRating.rating = content.rating
+        view.content.setOnClickListener {
+            onClick.invoke(content)
+        }
+
+        // TODO Получать из модели
+        Picasso.get()
+            .load("https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_.jpg")
+            .into(view.imagePreview)
+    }
+
+    override fun initializeViewBinding(v: View) = ItemTvShowBinding.bind(v)
+}
